f67df820e5aa1dfc9830dcbc6896960400fe4c6c
deni <krushitel_950@mail.ru>
2023-05-28 13:22:52 +0300

    обавил возможность разбавлять страницы на разные чанки

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from "react";
-import Counter from "./components/Counter";
+import React, { Suspense } from "react";
 import "./index.scss";
+import { Route, Routes, Link } from "react-router-dom";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 
 export const App = () => {
   return (
-    <div>
-      <Counter />
+    <div className="wrapper">
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О нас</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<MainPageAsync />} />
+          <Route path="/about" element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
