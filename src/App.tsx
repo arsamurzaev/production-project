22022fe05b2cf4c78488d9780661303cde38ac8a
deni <krushitel_950@mail.ru>
2023-05-28 14:56:09 +0300

    динамическое добавление классов

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React, {Suspense} from 'react';
-import {Link, Route, Routes} from 'react-router-dom';
-import './styles/index.scss';
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import {useTheme} from "./theme/useTheme";
+import React, { Suspense } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import "./styles/index.scss";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 const App = () => {
-    const {theme, toggleTheme} = useTheme();
+  const { theme, toggleTheme } = useTheme();
 
-    return (
-        <div className={`app ${theme}`}>
-            <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync />} />
-                    <Route path={'/'} element={<MainPageAsync />} />
-                </Routes>
-            </Suspense>
-        </div>
-    );
+  return (
+    <div
+      className={classNames("app", { hovered: true, selected: false }, [theme])}
+    >
+      <button onClick={toggleTheme}>TOGGLE</button>
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
 };
 
 export default App;
